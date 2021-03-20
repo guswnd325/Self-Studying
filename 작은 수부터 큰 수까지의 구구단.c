0750a96645b155f4e9a29e392e6b008afa9ab880
HyeonJoong <68773591+guswnd325@users.noreply.github.com>
2021-03-21 02:02:58 +0900

    Update 작은 수부터 큰 수까지의 구구단.c

--- 작은 수부터 큰 수까지의 구구단.c
+++ 작은 수부터 큰 수까지의 구구단.c
@@ -1,8 +1,25 @@
-#include <stdio.h> // 헤더파일 불러오기 
+#include <stdio.h>
 
-int main(void) { // 메인 함수 실행 
- 
-	printf("Hello"); // Hello 출력  
-	
-	return 0; // 0 반환 
+int main(void) {
+    int num1, num2, big, small, setnum;
+    printf("answer : ");
+    scanf("%d %d", &num1, &num2);
+    if (num1 > num2) {
+        big = num1;
+        small = num2;
+    }
+    else {
+        big = num2;
+        small = num1;
+    }
+    
+    setnum = small;
+    for (; setnum <= big; setnum++) {
+        for (int j = 1; j < 10; j++) {
+            printf("%d x %d = %d\n", setnum, j, (setnum * j));
+            }
+        printf("\n");
+    }
+    return 0;
 }
+
